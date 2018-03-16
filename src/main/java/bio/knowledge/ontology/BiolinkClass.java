bd28190d94ec6ba940a5b08ff4355541d007f1a0
Lance Hannestad <lhannest@sfu.ca>
2018-03-16 14:58:06 -0700

    Moved BiolinkClass to its own file - Deleted Library classes - Moved ModelLookup

---
+++ src/main/java/bio/knowledge/ontology/BiolinkClass.java
@@ -0,0 +1,165 @@
+package bio.knowledge.ontology;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BiolinkClass {
+	private String name;
+	private String description;
+	private String is_a;
+	private List<String> slots;
+	private Object slot_usage;
+	private Boolean mixin;
+	private List<String> flags;
+	@JsonProperty(value = "abstract") private Boolean _abstract;
+	private List<String> mixins;
+	@JsonProperty(value = "values_from") private List<String> values_from;
+	
+	@JsonProperty(value = "subclass_of") private String subclass_of;
+	private List<String> aliases = new ArrayList<String>();
+	@JsonProperty(value = "union_of") private List<String> union_of;
+	private List<String> mappings;
+	private String notes;
+	@JsonProperty(value = "id_prefixes") private List<String> id_prefixes;
+	private String comment;
+	@JsonProperty(value = "defining_slots") private List<String> defining_slots;
+	private Boolean symmetric;
+	@JsonProperty(value = "see_also") private String see_also;
+	private String schema;
+	@JsonProperty(value = "alt_descriptions") private List<String> alt_descriptions;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getIs_a() {
+		return is_a;
+	}
+	public void setIs_a(String is_a) {
+		this.is_a = is_a;
+	}
+	public List<String> getSlots() {
+		return slots;
+	}
+	public void setSlots(List<String> slots) {
+		this.slots = slots;
+	}
+	public Object getSlot_usage() {
+		return slot_usage;
+	}
+	public void setSlot_usage(Object slot_usage) {
+		this.slot_usage = slot_usage;
+	}
+	public Boolean getMixin() {
+		return mixin;
+	}
+	public void setMixin(Boolean mixin) {
+		this.mixin = mixin;
+	}
+	public List<String> getFlags() {
+		return flags;
+	}
+	public void setFlags(List<String> flags) {
+		this.flags = flags;
+	}
+	public Boolean isAbstract() {
+		return _abstract;
+	}
+	public void setIsAbstract(Boolean _abstract) {
+		this._abstract = _abstract;
+	}
+	public List<String> getMixins() {
+		return mixins;
+	}
+	public void setMixins(List<String> mixins) {
+		this.mixins = mixins;
+	}
+	public List<String> getValuesFrom() {
+		return values_from;
+	}
+	public void setValuesFrom(List<String> values_from) {
+		this.values_from = values_from;
+	}
+	public String getSubclassOf() {
+		return subclass_of;
+	}
+	public void setSubclassOf(String subclass_of) {
+		this.subclass_of = subclass_of;
+	}
+	public List<String> getAliases() {
+		return aliases;
+	}
+	public void setAliases(List<String> aliases) {
+		this.aliases = aliases;
+	}
+	public List<String> getUnionOf() {
+		return union_of;
+	}
+	public void setUnionOf(List<String> union_of) {
+		this.union_of = union_of;
+	}
+	public List<String> getMappings() {
+		return mappings;
+	}
+	public void setMappings(List<String> mappings) {
+		this.mappings = mappings;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public List<String> getIdPrefixes() {
+		return id_prefixes;
+	}
+	public void setIdPrefixes(List<String> id_prefixes) {
+		this.id_prefixes = id_prefixes;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public List<String> getDefiningSlots() {
+		return defining_slots;
+	}
+	public void setDefiningSlots(List<String> defining_slots) {
+		this.defining_slots = defining_slots;
+	}
+	public Boolean getSymmetric() {
+		return symmetric;
+	}
+	public void setSymmetric(Boolean symmetric) {
+		this.symmetric = symmetric;
+	}
+	public String getSeeAlso() {
+		return see_also;
+	}
+	public void setSeeAlso(String see_also) {
+		this.see_also = see_also;
+	}
+	public String getSchema() {
+		return schema;
+	}
+	public void setSchema(String schema) {
+		this.schema = schema;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return super.toString() + "[name=" + getName() + "]";
+	}
+}
--- src/main/java/bio/knowledge/ontology/mapping/ModelLookup.java
+++ src/main/java/bio/knowledge/ontology/mapping/ModelLookup.java
@@ -1,4 +1,4 @@
-package bio.knowledge.ontology;
+package bio.knowledge.ontology.mapping;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,7 +6,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import bio.knowledge.ontology.BiolinkModel.BiolinkClass;
+import bio.knowledge.ontology.BiolinkClass;
+import bio.knowledge.ontology.BiolinkModel;
 
 public class ModelLookup {
 	public static final String UMLS_PREFIX = "UMLS:";
@@ -16,7 +17,7 @@ public class ModelLookup {
 	public static void main(String[] args) {
 		ModelLookup lookup = new ModelLookup();
 		
-		BiolinkClass c = lookup.lookupRegex("gene production");
+		BiolinkClass c = lookup.lookupBestFit("DISO");
 		System.out.println(c.getName());
 	}
 	
@@ -28,7 +29,23 @@ public class ModelLookup {
 		}
 	}
 	
-	public BiolinkClass lookupRegex(String category) {
+	WikiDataBiolinkMapping wikidataMapping = new WikiDataBiolinkMapping();
+	UMLSBiolinkMapping umlsMapping = new UMLSBiolinkMapping();
+	
+	public String lookup(NameSpace namespace, String category) {
+		
+		return null;
+	}
+	
+	
+	/**
+	 * Finds the Biolink category with the shortest name that matches
+	 * the given category. Considers both the Biolink category's name
+	 * as well as aliases.
+	 * 
+	 * This method may return null if nothing fits.
+	 */
+	public BiolinkClass lookupBestFit(String category) {
 		Optional<BiolinkModel> opt = BiolinkModel.load();
 		BiolinkModel model = opt.get();
 		
@@ -36,8 +53,14 @@ public class ModelLookup {
 		
 		for (BiolinkClass biolinkClass : model.getClasses()) {
 			String name = biolinkClass.getName().toLowerCase().trim();
-			if (name.contains(category.toLowerCase().trim())) {
+			if (contains(name, category)) {
 				matches.add(biolinkClass);
+			} else {
+				for (String alias : biolinkClass.getAliases()) {
+					if (contains(alias, category)) {
+						matches.add(biolinkClass);
+					}
+				}
 			}
 		}
 		
@@ -56,5 +79,15 @@ public class ModelLookup {
 	
 	String umlsLookup(String umls) {
 		return null;
+	}
+	
+	/**
+	 * Performs a case insensitive check whether a is
+	 * contained in b or b is contained in a.
+	 */
+	private boolean contains(String a, String b) {
+		String lowerA = a.toLowerCase();
+		String lowerB = b.toLowerCase();
+		return lowerB.contains(lowerA) || lowerA.contains(lowerB);
 	}
 }
