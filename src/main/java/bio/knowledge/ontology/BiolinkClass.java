8a05b21e44b22980addb9e3a3160c79416958340
Richard Bruskiewich <richard@starinformatics.com>
2018-09-25 00:16:53 -0700

    try to test for Associations

--- src/main/java/bio/knowledge/ontology/BiolinkClass.java
+++ src/main/java/bio/knowledge/ontology/BiolinkClass.java
@@ -90,114 +90,143 @@ public class BiolinkClass implements BiolinkEntityInterface {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getIs_a() {
 		return is_a;
 	}
 	public void setIs_a(String is_a) {
 		this.is_a = is_a;
 	}
+
 	public List<String> getSlots() {
 		return slots;
 	}
 	public void setSlots(List<String> slots) {
 		this.slots = slots;
 	}
+
 	public Object getSlot_usage() {
 		return slot_usage;
 	}
 	public void setSlot_usage(Object slot_usage) {
 		this.slot_usage = slot_usage;
 	}
+
+	// Don't know if this is a unique tag in the model, but...
+    @JsonProperty(value = "subproperty_of") private String subproperty_of;
+
+    public String getSubproperty_of() {
+        return subproperty_of;
+    }
+    public void setSubproperty_of(String subproperty_of) {
+        this.subclass_of = subproperty_of;
+    }
+
 	public Boolean getMixin() {
 		return mixin;
 	}
 	public void setMixin(Boolean mixin) {
 		this.mixin = mixin;
 	}
+
 	public List<String> getFlags() {
 		return flags;
 	}
 	public void setFlags(List<String> flags) {
 		this.flags = flags;
 	}
+
 	public Boolean isAbstract() {
 		return _abstract;
 	}
 	public void setIsAbstract(Boolean _abstract) {
 		this._abstract = _abstract;
 	}
+
 	public List<String> getMixins() {
 		return mixins;
 	}
 	public void setMixins(List<String> mixins) {
 		this.mixins = mixins;
 	}
+
 	public List<String> getValuesFrom() {
 		return values_from;
 	}
 	public void setValuesFrom(List<String> values_from) {
 		this.values_from = values_from;
 	}
+
 	public String getSubclassOf() {
 		return subclass_of;
 	}
 	public void setSubclassOf(String subclass_of) {
 		this.subclass_of = subclass_of;
 	}
+
 	public List<String> getAliases() {
 		return aliases;
 	}
 	public void setAliases(List<String> aliases) {
 		this.aliases = aliases;
 	}
+
 	public List<String> getUnionOf() {
 		return union_of;
 	}
 	public void setUnionOf(List<String> union_of) {
 		this.union_of = union_of;
 	}
+
 	public List<String> getMappings() {
 		return mappings;
 	}
 	public void setMappings(List<String> mappings) {
 		this.mappings = mappings;
 	}
+
 	public String getNotes() {
 		return notes;
 	}
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
+
 	public List<String> getIdPrefixes() {
 		return id_prefixes;
 	}
 	public void setIdPrefixes(List<String> id_prefixes) {
 		this.id_prefixes = id_prefixes;
 	}
+
 	public String getComment() {
 		return comment;
 	}
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+
 	public List<String> getDefiningSlots() {
 		return defining_slots;
 	}
 	public void setDefiningSlots(List<String> defining_slots) {
 		this.defining_slots = defining_slots;
 	}
+
 	public Boolean getSymmetric() {
 		return symmetric;
 	}
 	public void setSymmetric(Boolean symmetric) {
 		this.symmetric = symmetric;
 	}
+
 	public String getSeeAlso() {
 		return see_also;
 	}
 	public void setSeeAlso(String see_also) {
 		this.see_also = see_also;
 	}
+
 	public String getSchema() {
 		return schema;
 	}
